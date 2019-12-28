66db6211b906ab343d7af7fde9697d8f6e105989
Sebastian Dudas <sebastian.frontend@gmail.com>
2019-12-28 19:42:44 +0100

    Add files

---
+++ APIGateway/Middlewares/TokenValidationMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using APIGateway.Consts;
+using Commons.ExceptionHandling.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGateway.Middlewares {
+    public class TokenValidationMiddleware {
+
+        public static void ValidateToken(String token) {
+            if (token != null) {
+                // token exists and needs to be validated
+                // TODO: add logic for calling the Auth Resource to validate the token
+                // [...]
+                // [...]
+                // [...]
+            } else {
+                // token does not exist
+
+                throw new UnauthenticatedException("You must provide a token to access this route!", GeneralConsts.MICROSERVICE_NAME);
+            }
+        }
+
+    }
+}
---
+++ Commons/ExceptionHandling/Exceptions/UnauthenticatedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Commons.ExceptionHandling.Exceptions {
+    public class UnauthenticatedException : BaseException {
+
+        public UnauthenticatedException(string message, string origin) : base(message, origin) {
+            this.message = message;
+            this.origin = origin;
+        }
+
+    }
+}
