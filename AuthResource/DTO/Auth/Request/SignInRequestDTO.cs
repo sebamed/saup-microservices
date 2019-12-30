67859f0a9f3e10fff90107992223a9a0a2af22b5
Sebastian Dudas <sebastian.frontend@gmail.com>
2019-12-30 22:10:18 +0100

    Added basic request validation in AuthResource.SignInRequestDTO

--- AuthResource/DTO/Auth/Request/SignInRequestDTO.cs
+++ AuthResource/DTO/Auth/Request/SignInRequestDTO.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AuthResource.DTO.User {
     public class SignInRequestDTO {
 
+        [EmailAddress]
+        [Required]
         public string username { get; set; }
 
+        [Required]
         public string password { get; set; }
 
     }
