0b33ee9c44e5799423e40dbd95c69775253eef81
Nikola Samardzija <nikola.samardzija.5@gmail.com>
2020-01-10 16:43:06 +0100

    Implemented File Microservice Access methods Get all files Get file by uuid Delete file Update file Create file Get file data by path

---
+++ FileMicroservice/DTO/File/Request/UpdateFileRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using Commons.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileMicroservice.DTO.File.Request {
+    public class UpdateFileRequestDTO: BaseDTO {
+
+        [Required]
+        public string fileData { get; set; }
+
+        [Required]
+        public string fileExtension { get; set; }
+
+    }
+}
