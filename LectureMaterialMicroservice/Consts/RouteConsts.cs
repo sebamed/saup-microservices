41f078ce55686098bac36c2fdf57b316347c85dc
Marko Popić <popic.mm@gmail.com>
2020-01-11 22:02:47 +0100

    Changed base route

--- LectureMaterialMicroservice/Consts/RouteConsts.cs
+++ LectureMaterialMicroservice/Consts/RouteConsts.cs
@@ -8,16 +8,16 @@ namespace LectureMaterialMicroservice.Localization {
 
         public const string ROUTE_API_BASE = "/api";
 
-        public const string ROUTE_SECTION_BASE = ROUTE_API_BASE + "/lecture-materials";
+        public const string ROUTE_SECTION_BASE = ROUTE_API_BASE + "/sections";
         public const string ROUTE_SECTION_GET_ONE_BY_UUID = ROUTE_SECTION_BASE + "/{uuid}";
         public const string ROUTE_SECTION_GET_VISIBLE = ROUTE_SECTION_BASE + "/visible";
         public const string ROUTE_SECTION_GET_BY_COURSE = ROUTE_SECTION_BASE + "/course/{uuid}";
         public const string ROUTE_SECTION_GET_VISIBLE_BY_COURSE = ROUTE_SECTION_GET_VISIBLE + "/course/{uuid}";
 
-        public const string ROUTE_SECTION_ARCHIVE_BASE = ROUTE_API_BASE + "/lecture-materials/archive";
+        public const string ROUTE_SECTION_ARCHIVE_BASE = ROUTE_API_BASE + "/sections/archive";
         public const string ROUTE_ARCHIVES_BY_SECTION_UUID = ROUTE_SECTION_ARCHIVE_BASE + "/{uuid}";
         public const string ROUTE_LATEST_ARCHIVE_BY_SECTION_UUID = ROUTE_SECTION_ARCHIVE_BASE + "/latest/{uuid}";
 
-        public const string ROUTE_MATERIAL_BASE = ROUTE_API_BASE + "/lecture-materials/material";
+        public const string ROUTE_MATERIAL_BASE = ROUTE_API_BASE + "/sections/material";
     }
 }
