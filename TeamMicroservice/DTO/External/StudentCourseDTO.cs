a1e35e3225dc8e46b3f99a407db389bf7abd42fe
Marko Popić <popic.mm@gmail.com>
2020-01-14 22:25:39 +0100

    Hotfix

---
+++ TeamMicroservice/DTO/External/StudentCourseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamMicroservice.DTO.External
+{
+    public class StudentCourseDTO
+    {
+        public string studentUUID { get; set; }
+        public string courseUUID { get; set; }
+    }
+}
